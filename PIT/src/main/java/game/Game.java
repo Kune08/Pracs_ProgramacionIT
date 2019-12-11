00c16c6556300a016788c91d3bb87c31c40f353d
Andres <andres.ruz@edu.upct.es>
2019-12-11 17:36:26 +0100

    Juego

--- PIT/src/main/java/game/Game.java
+++ PIT/src/main/java/game/Game.java
@@ -1,8 +1,4 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+// Desplegar
 // _____                 _                           _             _                       
 //|_   _|               | |                         | |           (_)                      
 //  | |  _ __ ___  _ __ | | ___ _ __ ___   ___ _ __ | |_ __ _  ___ _  ___  _ __   ___  ___ 
